72801e089ad3f41b92137bd5d5fa726944f38bcc
Holly <162161116+Holl4444@users.noreply.github.com>
2025-02-07 19:56:44 +0000

    Add text to handle minutes on Timer button

--- Tenzies/src/Timer/Timer.jsx
+++ Tenzies/src/Timer/Timer.jsx
@@ -1,7 +1,13 @@
 export default function Timer(props) {
   return (
     <button onClick={props.fn} ref={props.timerElement} id="timer">
-      {props.timer > 0 ? props.timer : 'Start Timer'}
+      {props.timer > 0
+        ? props.timer > 59
+          ? `${Math.floor(props.timer / 60)}m ${props.timer % 60}s`
+          : `${props.timer}s`
+        : 'Start Timer'}
     </button>
   );
 }
+
+
