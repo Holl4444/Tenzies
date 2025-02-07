d06047678729f50f00d4c76077c16bd5f27b9332
Holly <162161116+Holl4444@users.noreply.github.com>
2025-02-07 21:56:27 +0000

    Styling

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -48,7 +48,7 @@ export default function App() {
     timerElement.current = 0;
     setSeconds(0);
   };
-  
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
@@ -101,23 +101,29 @@ export default function App() {
           </p>
         )}
       </div>
-      <h1 className="title">Tenzies</h1>
-      <p className="instructions">
-        Roll until all dice are the same. Click each die to freeze it
-        at its current value between rolls.
-      </p>
-      <div className="dice-container">{diceElements}</div>
+      <div className="content">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze
+          it at its current value between rolls.
+        </p>
+        <div className="dice-container">{diceElements}</div>
 
-      <button ref={rollBtn} className="roll-dice" onClick={rollDice}>
-        {gameWon ? 'New Game' : 'Roll'}
-      </button>
-      <div className="actions-div">
-        <Timer
-          timerElement={timerElement}
-          timer={seconds}
-          fn={setTimer}
-        />
-        <Counter state={count} />
+        <button
+          ref={rollBtn}
+          className="roll-dice"
+          onClick={rollDice}
+        >
+          {gameWon ? 'New Game' : 'Roll'}
+        </button>
+        <div className="actions-div">
+          <Timer
+            timerElement={timerElement}
+            timer={seconds}
+            fn={setTimer}
+          />
+          <Counter state={count} />
+        </div>
       </div>
     </main>
   );
