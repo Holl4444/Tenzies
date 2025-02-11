92892b890d2e8f9f45bcf7ca02fba1d1a5beeae0
Holl4444 <holly.cello@gmail.com>
2025-02-11 16:28:47 +0000

    sentry

--- Tenzies/src/Timer/Timer.jsx
+++ Tenzies/src/Timer/Timer.jsx
@@ -7,17 +7,26 @@ export default function Timer(props) {
     //Start timer button, text becomes the timer itself until the timer is reset
     //Add simple time formatting
     //disable button after timer started
-    <button
-      disabled={props.timer && 'disabled'}
-      onClick={handleClick}
-      ref={props.timerElement}
-      id={'timer'}
-    >
-      {props.timer > 0
-        ? props.timer > 59
-          ? `${Math.floor(props.timer / 60)}m ${props.timer % 60}s`
-          : `${props.timer}s`
-        : 'Start Timer'}
-    </button>
+    <>
+      <button
+        disabled={props.timer && 'disabled'}
+        onClick={handleClick}
+        ref={props.timerElement}
+        id={'timer'}
+      >
+        {props.timer > 0
+          ? props.timer > 59
+            ? `${Math.floor(props.timer / 60)}m ${props.timer % 60}s`
+            : `${props.timer}s`
+          : 'Start Timer'}
+      </button>
+      <button
+        onClick={() => {
+          throw new Error('This is your first error!');
+        }}
+      >
+        Break the world
+      </button>
+    </>
   );
 }
